814f39080ca22b2f21420b7e178717a3b2d878d8
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2023-11-28 10:48:29 -0300

    removing comments from food delivery

--- 02-OOP/05-Food-Delivery-Day-Two/01-Food-Delivery/app.rb
+++ 02-OOP/05-Food-Delivery-Day-Two/01-Food-Delivery/app.rb
@@ -1,6 +1,5 @@
 # TODO: require relevant files to bootstrap the app.
 # Then you can test your program with:
-#   ruby app.rb
 require_relative "app/repositories/meal_repository.rb"
 require_relative "app/controllers/meals_controller.rb"
 

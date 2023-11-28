b7fbe74c3478b3ffa5799b9611acea7b8c680302
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2023-11-28 10:49:32 -0300

    removing comments from food delivery

--- 02-OOP/05-Food-Delivery-Day-Two/01-Food-Delivery/router.rb
+++ 02-OOP/05-Food-Delivery-Day-Two/01-Food-Delivery/router.rb
@@ -18,14 +18,12 @@ class Router
     @user = @employees_controller.login
     while @running
       if @user.manager?
-        #if your role is 'manager'
         puts "Welcome to the manager area"
         display_tasks_manager
         action = gets.chomp.to_i
         print `clear`
         route_action_manager(action)
-      else #or elsif @user.rider?
-        #if your role is rider
+      else
         puts "Welcome to the rider area"
         display_tasks_rider
         action = gets.chomp.to_i

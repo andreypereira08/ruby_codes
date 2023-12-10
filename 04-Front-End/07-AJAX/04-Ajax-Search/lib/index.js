a2878500cd64b60804a9779c77bef444fb1c3b94
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2023-12-10 10:28:18 -0300

    fix api result - Ajax Search

--- 04-Front-End/07-AJAX/04-Ajax-Search/lib/index.js
+++ 04-Front-End/07-AJAX/04-Ajax-Search/lib/index.js
@@ -1,5 +1,6 @@
 const omdbapiUrl = "http://www.omdbapi.com/";
-const apiKey = "adf1f2d7";
+// const apiKey = "adf1f2d7";
+const apiKey = "8691812a";
 
 // Here is 2 other API key if the one above doesn't work anymore:
 // - 48727053

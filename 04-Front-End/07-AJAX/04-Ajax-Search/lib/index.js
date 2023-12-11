814b77731cca3f4b4bddc7c0a510073839c86c99
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2023-12-11 08:48:32 -0300

    Update index.js

--- 04-Front-End/07-AJAX/04-Ajax-Search/lib/index.js
+++ 04-Front-End/07-AJAX/04-Ajax-Search/lib/index.js
@@ -1,4 +1,4 @@
-const omdbapiUrl = "http://www.omdbapi.com/";
+const omdbapiUrl = "https://www.omdbapi.com/";
 const apiKey = "adf1f2d7";
 
 // Here is 2 other API key if the one above doesn't work anymore:

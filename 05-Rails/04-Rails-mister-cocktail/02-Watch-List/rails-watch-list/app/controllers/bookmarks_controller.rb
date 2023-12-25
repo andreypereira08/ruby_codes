adfbcf678b94f7f7db46ab8f51cedaa5b3198b68
andrey pereira <andrey08.ap@gmail.com>
2023-12-25 15:11:47 -0300

    Adjusting frontend - Rails Watch List

--- 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/app/controllers/bookmarks_controller.rb
+++ 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/app/controllers/bookmarks_controller.rb
@@ -1,14 +1,13 @@
 class BookmarksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-
     if @bookmark.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
--- 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/config/routes.rb
+++ 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lists, only: [:index, :new, :create, :show] do
-    resources :bookmarks, only: [:new, :create, :destroy, :new]
+    resources :bookmarks, only: [:new, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

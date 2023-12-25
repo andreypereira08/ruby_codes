e66747a8e75803b13ad0d890df4df144c5e8761d
Andrey <andrey08.ap@gmail.com>
2023-12-25 11:33:22 -0300

    Implement image upload - Rails Watch List

--- 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/app/controllers/lists_controller.rb
+++ 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/app/controllers/lists_controller.rb
@@ -25,6 +25,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :banner)
+    params.require(:list).permit(:name, :banner, :photo)
   end
 end
--- 05-Rails/04-Rails-mister-cocktail/02-Watch-List/rails-watch-list/db/migrate/20231223171925_add_banner_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddBannerToLists < ActiveRecord::Migration[7.1]
-  def change
-    add_column :lists, :banner, :string
-  end
-end
